c7dec24b32c164f10ba67313bc90e81234886506
Murad Aghazada <tariyelagazade@Murads-MacBook-Pro3.local>
2023-05-03 16:13:38 +0400

    m

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from accounts.views import test_account
+from accounts.views import test_account, register
 
 app_name = 'accounts'
 
 
 urlpatterns = [
     path('profile/<int:id>',test_account,name ='test_account'),
+    path('register/',register,name ='register'),
+
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from accounts.models import User
 
 def test_account(request, id):
@@ -10,3 +10,13 @@ def test_account(request, id):
     return render(request, 'test.html', context)
 
 
+def register(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        firstname = request.POST.get('firstname')
+        lastname = request.POST.get('lastname')
+        password = request.POST.get('password')
+        user = User.objects.create(username=username, first_name=firstname, last_name=lastname, password=password)
+        user.save()
+        return redirect('/')
+    return render(request, 'register.html')
